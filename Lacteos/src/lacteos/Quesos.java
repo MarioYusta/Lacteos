b918642c15ab283cb01e20d9341747839c095780
MarioYS <m.yusta@fp.safavalladolid.com>
2024-06-05 13:04:39 +0200

    Refatorizado el nombre de imprimir a imprimir_cabecera

--- Lacteos/src/lacteos/Quesos.java
+++ Lacteos/src/lacteos/Quesos.java
@@ -50,7 +50,7 @@ public class Quesos {
 		this.peso = peso;
 	}
 	
-	public void imprimir () {
+	public void imprimir_cabecera () {
 		System.out.println("QUESOS ENTREPINARES");
 		System.out.println("Registro Sanitario No 52/38751");
 		System.out.println("Para consultar el lote del producto revise la etiqueta");
